4ebfc99fa4323e7675963e414e374343ed2b446f
Brian Leonard <brian.leonard@mongodb.com>
2020-08-18 18:47:40 -0400

    Added example using ServerAddress connection style

--- run.sh
+++ run.sh
@@ -1,4 +1,18 @@
-export mongodb_uri="mongodb://localhost:27017"
+# Options include "URI" (default) or "ServerAddress" (old style)
+# export mongodb_connection_style="ServerAddress"
+
+# Set if using URI Connection Style
+# export mongodb_uri="mongodb://localhost:27017"
+export mongodb_uri="<Atlas Connection String>"
+
+# Set if using ServerAddress Connection Style
+export server="<Primary Node>"
+export port=27017
+export username="<username>"
+export password="<password>"
+
 java -cp target/java-bootstrap-1.0-SNAPSHOT-jar-with-dependencies.jar com.mongodb.java.bootstrap.App
+
+
 
 
--- src/main/java/com/mongodb/java/bootstrap/App.java
+++ src/main/java/com/mongodb/java/bootstrap/App.java
@@ -1,8 +1,18 @@
 package com.mongodb.java.bootstrap;
 
 import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoClient;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mongodb.MongoDriverInformation;
+import com.mongodb.MongoCredential;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.ServerAddress;
+import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.internal.build.MongoDriverVersion;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 
@@ -10,24 +20,67 @@ import org.bson.Document;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("MongoDB Java Bootstrap");
+        System.out.println("\n*** MongoDB Java Bootstrap ***");
 
-        // Grab the connection string from the environment:
-        String uriString = System.getenv("mongodb_uri");
-        
-        if (uriString == null) {
-            System.out.println("MongoDB Connection String environment variable, 'mongodb_uri', is not set");
-            return;
+        String mongoDriverVersion  = MongoDriverVersion.VERSION;
+        System.out.println("\n*** MongoDB Driver Version: " + mongoDriverVersion + " ***");
+
+        String connStyle = System.getenv("mongodb_connection_style");
+        //System.out.println( "mongodb_connection_style: " + connStyle);
+
+        String uriString = null;
+        String serverName = null;
+        int port = 27017;
+        String username = null;
+        String password = null;
+
+        if (connStyle != null && connStyle.equals("ServerAddress")) {
+            System.out.println("\n*** Using ServerAddress connection style ***\n");
+            serverName = System.getenv("server");
+            port = Integer.parseInt(System.getenv("port"));
+            username = System.getenv("username");
+            password = System.getenv("password");
+        } else {
+            System.out.println("\n*** Using URI connection style ***\n");
+            // Grab the connection string from the environment:
+            uriString = System.getenv("mongodb_uri");
+            
+            if (uriString == null) {
+                System.out.println("MongoDB Connection String environment variable, 'mongodb_uri', is not set");
+                return;
+            }
         }
 
         //String uriString = "mongodb://localhost:27017";
         String database_string = "sample_analytics";
         String collection_string = "customers";
+        MongoDatabase database = null;
 
-        MongoClient mongoClient = MongoClients.create(uriString);
-        MongoDatabase database = mongoClient.getDatabase(database_string);
+        if (connStyle == null || connStyle.equals("URI")) {
+            com.mongodb.client.MongoClient mongoClient = MongoClients.create(uriString);
+            database = mongoClient.getDatabase(database_string);
+        } else {
+
+            ServerAddress serverAddress = new ServerAddress(serverName, port);
+            List<ServerAddress> addressList = Arrays.asList(new ServerAddress[] { serverAddress });
+            System.out.println("addressList: " + addressList);      
+            
+            List<MongoCredential> mongoCredentialList = new ArrayList();
+            MongoCredential mongoCredential = MongoCredential.createScramSha1Credential(username, "admin", password.toCharArray());
+    
+            MongoClientOptions.Builder options = new MongoClientOptions.Builder();
+            options.sslEnabled(true);
+            
+            com.mongodb.MongoClient mongoClient = new MongoClient(addressList, mongoCredential, options.build());
+            database = mongoClient.getDatabase(database_string);
+
+        }
+
+        
         MongoCollection<Document> collection = database.getCollection(collection_string);
-        Document myDoc = collection.find().first();
-        System.out.println(myDoc);
+        Document myDoc = collection.find().first();   
+
+        System.out.println("\nResults: ");
+        System.out.println("\n" + myDoc + "\n");
     }
 }
